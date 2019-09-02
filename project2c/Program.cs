60f310efb33720c3bbd730de77b149056b76c9d4
lvoovna <julia5474j@gmail.com>
2019-09-02 19:22:48 +0300

    Add project files.

---
+++ project2c/Program.cs
@@ -0,0 +1,188 @@
+﻿/*
+using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x,y,s;
+            Console.WriteLine("Write x");
+            x = Convert.ToInt32(Console.ReadLine());
+            s = x * x + 5 * x + 8;
+            y = Math.Sqrt(s);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+/*
+ using System;
+
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y ;
+            Console.WriteLine("Write x");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = (3 * x * x - 4) / (x + 1);
+            Console.WriteLine("y=" + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+/*using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y,s;
+            Console.WriteLine("Write x");
+            x = Convert.ToInt32(Console.ReadLine());
+            s = x + 2;
+            y = x- Math.Sqrt(s);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+/*
+using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x,y;
+            Console.WriteLine("Write x");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = (7 * x + 4) /( 3 * x + 5) + Math.Cos(x);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+/*using System;
+
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Write x");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = x * x * x + 4 *x* x + x - 2;
+            Console.WriteLine("y=" + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+/*
+using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Write x");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = x * Math.Sin(3 * x);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+/*
+using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Write x");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Math.Cos(x) + Math.Sqrt(x);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+
+
+
+/*
+using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Write x");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Math.Cos(x) + Math.Sqrt(x);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+/*
+using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Write x");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = (Math.Sin(x) / 3) + (Math.Tan(x) / 6);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+*/
+using System;
+namespace project2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, s,w,z;
+            Console.WriteLine("Write x");
+            x = Convert.ToInt32(Console.ReadLine());
+            s = x + 2;
+            w = x + 3;
+            z= Math.Sqrt(s);
+            y = z-Math.Sqrt(w);
+            Console.WriteLine("y= " + y);
+            Console.ReadLine();
+        }
+    }
+}
+
